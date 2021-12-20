8a9decb6021a798bedec7aadfa1874286e7b56fd
gyuqian <2537018327@qq.com>
2021-12-20 18:24:05 +0800

    修改倾角传感器数据返回方式

--- src/angleSensor.cpp
+++ src/angleSensor.cpp
@@ -17,17 +17,14 @@ int ReadData(int chn)
 {
 	if(chn == 0)
 	{
-		//wiringPiI2CWrite(fd,0x44);
 		return wiringPiI2CReadReg8(fd,0x44);
 	}
 	else if(chn == 1)
 	{
-		//wiringPiI2CWrite(fd,0x45);
 		return wiringPiI2CReadReg8(fd,0x45);
 	}
 	else if(chn == 2)
 	{
-		//wiringPiI2CWrite(fd,0x46);
 		return wiringPiI2CReadReg8(fd,0x46);
 	}
 	else
